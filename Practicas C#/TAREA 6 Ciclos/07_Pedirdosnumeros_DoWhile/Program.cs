ce5c0c68998f08ce9038640c5d6a73ac815613a5
KGAYOTO24 <manuelstarling2006@gmail.com>
2026-09-18 21:01:04 -0400

    Add files via upload

---
+++ Practicas C#/TAREA 6 Ciclos/07_Pedirdosnumeros_DoWhile/Program.cs
@@ -0,0 +1,26 @@
+﻿// EJERCICIO 7 - DO WHILE Secuencia entre dos números
+
+Console.Write("Ingrese el primer número: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Ingrese el segundo número: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+if (num1 > num2)
+{
+    do
+    {
+        Console.WriteLine(num1);
+        num1--;
+    }
+    while (num1 >= num2);
+}
+else
+{
+    do
+    {
+        Console.WriteLine(num1);
+        num1++;
+    }
+    while (num1 <= num2);
+}
---
+++ Practicas C#/TAREA 6 Ciclos/07_Pedirdosnumeros_While/Program.cs
@@ -0,0 +1,24 @@
+﻿//EJERCICIO 7 - WHILE Secuencia entre dos números
+
+Console.Write("Ingrese el primer número: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Ingrese el segundo número: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+if (num1 > num2)
+{
+    while (num1 >= num2)
+    {
+        Console.WriteLine(num1);
+        num1--;
+    }
+}
+else
+{
+    while (num1 <= num2)
+    {
+        Console.WriteLine(num1);
+        num1++;
+    }
+}
