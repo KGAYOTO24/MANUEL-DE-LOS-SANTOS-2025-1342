5eed5d358a39032fe1c8fc3c007cc77957917514
KGAYOTO24 <manuelstarling2006@gmail.com>
2026-09-18 20:17:36 -0400

    Add files via upload

---
+++ Practicas C#/12_Intercambiar_variables/Program.cs
@@ -0,0 +1,20 @@
+﻿// Intercambiar variables
+
+{
+
+ Console.WriteLine("Escribe el valor de A:");
+ string varA = Console.ReadLine()!;
+ Console.WriteLine("Escribe el valor de B:");
+ string varB = Console.ReadLine()!;
+
+
+ string temporal = varA;
+ varA = varB;
+ varB = temporal;
+
+ Console.WriteLine("Ahora A vale: " + varA);
+ Console.WriteLine("Ahora B vale: " + varB);   
+
+Console.ReadKey();
+
+}
---
+++ Practicas C#/13_Iniciales/Program.cs
@@ -0,0 +1,17 @@
+﻿// Iniciales
+
+{
+    Console.WriteLine("¿Cual es tu nombre?");
+string n = Console.ReadLine()!;
+Console.WriteLine("¿Primer apellido?");
+string p1 = Console.ReadLine()!;
+Console.WriteLine("¿Segundo apellido?");
+string p2 = Console.ReadLine()!;
+
+
+string iniciales = n[0] + "" + p1[0] + "" + p2[0];
+Console.WriteLine("Tus iniciales son: " + iniciales.ToUpper());
+
+
+    Console.ReadKey();
+}
