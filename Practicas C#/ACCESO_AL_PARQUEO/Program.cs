dffa64268c6a00cdcb317826fad4f399b0015d1a
KGAYOTO24 <manuelstarling2006@gmail.com>
2026-09-18 20:08:41 -0400

    Add files via upload

---
+++ Practicas C#/ACCESO_AL_PARQUEO/Program.cs
@@ -0,0 +1,23 @@
+﻿// PROGRAMA QUE SIMULA EL CONTROL DE ACCESO A UN PARQUEO
+
+using System.Security.AccessControl;
+using System.Security.Cryptography;
+
+
+Console.Write("Podria facilitar su nombre? ");
+string nombre = Console.ReadLine()!;
+
+Console.WriteLine("Que vehiculo esta conduciendo? ");
+string tipo = Console.ReadLine()!;
+
+Console.WriteLine("Tienes un pase de acceso? ");
+string pase = Console.ReadLine()!;
+
+
+if (tipo == "Carro" && pase == "si"  )
+{
+    Console.WriteLine
+}
+
+
+
