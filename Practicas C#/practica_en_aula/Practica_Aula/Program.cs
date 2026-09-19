b40536a6709326015b8c8683a64fa681af9534c8
KGAYOTO24 <manuelstarling2006@gmail.com>
2026-09-18 20:07:02 -0400

    Add files via upload

---
+++ Practicas C#/practica_en_aula/Practica_Aula/Program.cs
@@ -0,0 +1,118 @@
+﻿
+using System.Collections;
+
+bool salir = false;
+
+while(!salir)
+System.Console.WriteLine(" === Menu === ");
+System.Console.WriteLine("1. Ver listado general de estudiantes. ");
+System.Console.WriteLine("2. Ver estadisticas del grupo. ");
+System.Console.WriteLine("3. Buscar estudiante por nombre");
+System.Console.WriteLine("4. Ver estudiantes por condicion");
+System.Console.WriteLine("5. Salir");
+
+System.Console.Write("Elige una opcion: ");
+int opcion = int.Parse(Console.ReadLine()!);
+
+switch (opcion)
+{
+    case 1:
+    RegistrarDatos(nombre)
+    break;
+
+    case 2:
+    break;
+
+    case 3:
+    break;
+
+    case 4:
+    break;
+
+    case 5:
+        salir = true;
+    break;
+    
+    
+}
+
+
+
+static void RegistrarDatos(String[] NombreEstudiante, int[] calificacionEstudiante, int cantidadEstudiante)
+
+{
+    for (int i = 0; i < cantidadEstudiante; i++)
+
+    System.Console.Write("Cuantos estudiantes quiere ingresar: ");
+    cantidadEstudiante = int.Parse(Console.ReadLine()!);
+
+    System.Console.Write("Nombre del estudiante: ");
+    NombreEstudiante[cantidadEstudiante] = Console.ReadLine()!;
+
+    System.Console.WriteLine("Ingresa calificacion ");
+    calificacionEstudiante[cantidadEstudiante] = int.Parse(Console.ReadLine()!);
+
+
+}
+static void MostrarDatos(String[] NombreEstudiante, int[] calificacionEstudiante, int cantidadEstudiante)
+{
+    
+    for(int i = 0; i < cantidadEstudiante; i++)
+
+    {
+        System.Console.WriteLine($"Nombre del estudiante: {NombreEstudiante[i]}");
+        System.Console.WriteLine($"calificacion: {calificacionEstudiante}");
+
+        if(calificacionEstudiante[i] > 70)
+        {
+            System.Console.WriteLine("Estado: Aprobado ");
+        }
+        else
+        {
+            System.Console.WriteLine("Estado: Repobrado ");
+        }
+    }
+
+}
+static void EstadisticaGrupo(string[] nombreEstudiante, int[] Calificaciones, int cantidadEstudiante)
+{
+    int promedio = 0;
+
+    for(int i = 0; i < cantidadEstudiante; i++);
+    promedio = Calificaciones[cantidadEstudiante];
+
+    if(promedio > Calificaciones[cantidadEstudiante])
+    {
+        System.Console.WriteLine("Es mayor ");
+    }
+    else
+    {
+        System.Console.WriteLine("Es menor");
+    }
+    System.Console.WriteLine($"Nombre del estudiante {nombreEstudiante[cantidadEstudiante]} - calificacion {Calificaciones[cantidadEstudiante]}");
+
+
+        
+    
+}
+
+
+static void BuscarEstudiante(String[] nombreEstudiante, int[] calificaciones, int cantidadEstudiante)
+{
+    string buscar;
+
+    System.Console.Write("Que estudiante quiere buscar: ");
+    buscar = Console.ReadLine()!;
+
+    for(int i = 0; i < cantidadEstudiante; i++)
+
+    if(nombreEstudiante[cantidadEstudiante].ToLower() == buscar.ToLower())
+    {
+        System.Console.WriteLine($"Nombre del estudiante {nombreEstudiante[i]} - Calificacion {calificaciones[i]} ");
+    }
+
+}
+
+
+
+
---
+++ Practicas C#/practica_en_aula/Program.cs
@@ -0,0 +1,73 @@
+﻿// Sistema Academico basico
+
+/*El programa debe pedir al usuario, pedirle 4 notas y mostrar el litar correspondiente al primario (A)=>90 (B)>= 80 (C)>=70 (F)<= 70
+Dale 2 opcion al usuario, Ver el literal y ver el promedio*/
+
+
+
+int nota1, nota2, nota3, nota4, promedio, opcion_menu;
+
+System.Console.WriteLine("Ingrese la nota 1 ");
+nota1 = int.Parse (Console.ReadLine()!);
+
+System.Console.WriteLine("Ingrese la nota 2 ");
+nota2 = int.Parse (Console.ReadLine()!);
+
+System.Console.WriteLine("Ingrese la nota 3 ");
+nota3 = int.Parse (Console.ReadLine()!);
+
+System.Console.WriteLine("Ingrese la nota 4 ");
+nota4 = int.Parse (Console.ReadLine()!);
+
+promedio = (nota1+nota2+nota3+nota4)/4;
+
+Console.WriteLine("Elige una opciones ");
+Console.WriteLine("1. Ver literal ");
+Console.WriteLine("2. Ver promedio ");
+opcion_menu = int.Parse(Console.ReadLine()!);
+
+switch (opcion_menu)
+{
+    case 1:
+    if (promedio >= 90)
+        {
+            System.Console.WriteLine($"A {promedio}, Felicidades! ");
+            }
+        else if (promedio >= 80)
+        {
+            System.Console.WriteLine($"B {promedio}, Felicidades! ");
+        }
+        else if(promedio >= 70)
+        {
+            System.Console.WriteLine($"C {promedio}, Felicidades! ");
+        }
+        else if(promedio < 70)
+        {
+            System.Console.WriteLine($"F {promedio}, Siguelo intentando! ");
+        }
+        break;
+    
+    case 2:
+    
+    System.Console.WriteLine($"Tu promedio es: {promedio}");
+    break;
+
+    default:
+    System.Console.WriteLine("Opcion no valida ");
+    break;
+
+        
+        
+        
+    
+            
+            
+    
+
+
+    
+}
+             
+// 90+95+93+91/4
+
+
