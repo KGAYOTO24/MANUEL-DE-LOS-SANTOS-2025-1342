8ed4e9c32fd6233b9fdc296d3f97497f07dcc4fd
KGAYOTO24 <manuelstarling2006@gmail.com>
2026-09-18 20:09:33 -0400

    Add files via upload

---
+++ Practicas C#/Calculadora_basica/Program.cs
@@ -0,0 +1,55 @@
+﻿// Programa que simula una calculadora
+
+using System;
+
+class Programa
+{
+    static void Main()
+    {
+        int num1, num2;
+        string operacion;
+        int resultado = 0;
+
+        Console.Write("inserta el primer numero: ");
+        num1 = int.Parse(Console.ReadLine()!);
+
+        Console.Write("inserta el segundo numero: ");
+        num2 = int.Parse(Console.ReadLine()!);
+
+        Console.Write("inserta la operacion que quieres realizar (+, -, *, /): ");
+        operacion = Console.ReadLine()!;
+
+        if (operacion == "+")
+        {
+            resultado = num1 + num2;
+        }
+        else if (operacion == "-")
+        {
+            resultado = num1 - num2;
+        }
+        else if (operacion == "*")
+        {
+            resultado = num1 * num2;
+        }
+        else if (operacion == "/")
+        {
+            if (num2 != 0)
+            {
+                resultado = num1 / num2;
+            }
+            else
+            {
+                Console.WriteLine("no puedes dividir entre 0");
+                return;
+            }
+        }
+        else
+        {
+            Console.WriteLine("operacion no valida");
+            return;
+        }
+
+        Console.WriteLine("Resultado: " + resultado);
+    }
+}
+//MANUEL STARLING DE LOS SANTOS CASTRO
---
+++ Practicas C#/nombre_edad/Program.cs
@@ -0,0 +1,12 @@
+﻿//programa que te da tu nombre y edad
+
+Console.Write("Ingresa tu nombre: ");
+string nombre = Console.ReadLine()!;
+
+Console.Write("Ingresa tu edad ");
+int edad = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Hola "  + nombre +", tienes " + edad + " años");
+
+
+
