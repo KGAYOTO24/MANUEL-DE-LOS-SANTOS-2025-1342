773cbce156967e81ea4911f00746bd418d46f75a
KGAYOTO24 <manuelstarling2006@gmail.com>
2026-09-18 20:10:38 -0400

    Add files via upload

---
+++ Practicas C#/programa_alam/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Crear un programa que pida al usuario, una cotraseña (contraseña 1234), 
+el usuario tiene 3 intentos para colocar la contraseña en caso de que se agoten los 3 intentos
+el programa mostrara acceso denegado, en el caso de que la contraseña sea correcta, que aparezca
+Bienvenido al programa.*/
+
+string contraseñaCorrecta = "1234";
+        string contraseñaUsuario;
+
+        int intentos = 3;
+
+        while (intentos > 0)
+        {
+            Console.Write("Ingresa la contraseña: ");
+            contraseñaUsuario = Console.ReadLine()!;
+
+            if (contraseñaUsuario == contraseñaCorrecta)
+            {
+                Console.WriteLine("Bienvenido al programa");
+                break;
+            }
+            else
+            {
+                intentos--;
+                Console.WriteLine("Contraseña incorrecta. Intentos restantes: " + intentos);
+            }
+        }
+
+        if (intentos == 0)
+        {
+            Console.WriteLine("Acceso denegado");
+        }
---
+++ Practicas C#/sistema_de_menu/Program.cs
@@ -0,0 +1,58 @@
+﻿/* Realiza un programa en C# que simule un sistema de menú.
+
+El programa debe solicitar el nombre del usuario y mostrar un menú con varias opciones.
+
+El usuario podrá elegir opciones que sumen los puntos que desee o resten los puntos que desee a una variable acumuladora.
+
+El sistema debe repetirse hasta que el usuario decida salir.
+
+Al final, el programa debe mostrar el nombre del usuario y el total de puntos obtenidos.
+*/
+
+System.Console.Write("Ingresa tu nombre: ");
+string nombre = Console.ReadLine()!;
+
+int puntos = 0;
+int opcion;
+
+do
+{
+    System.Console.WriteLine("\n ==== MENU =====");
+    System.Console.WriteLine("1. Sumar 20 ");
+    System.Console.WriteLine("2. Restar 15 ");
+    System.Console.WriteLine("3. ver puntos acumulados ");
+    System.Console.WriteLine("4. Salir ");
+
+    opcion = int.Parse(Console.ReadLine()!);
+
+    switch (opcion)
+    {
+        case 1:
+            puntos += 20;
+            System.Console.WriteLine("sumaste 10 puntos ");
+            break;
+
+            case 2:
+            puntos -= 15;
+            System.Console.WriteLine("restaste 15 puntos ");
+            break;
+
+            case 3:
+            System.Console.WriteLine($"Los puntos que has acumulado son: {puntos} ");
+            break;
+
+            case 4:
+            System.Console.WriteLine($"{nombre}, Has decidido salir. Hasta luego! ");
+            break;
+
+            default:
+            System.Console.WriteLine("caracter no valido ");
+            break;
+
+
+    }
+}while (opcion != 4);
+
+System.Console.WriteLine("\n ====== Resultado Final ==== ");
+System.Console.WriteLine($"Jugador: {nombre}" );
+System.Console.WriteLine($"puntos acumulados: {puntos} ");
